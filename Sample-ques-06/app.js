5d251baba4de68957819245329301a42105d9ec4
RuchaRK <ruchakathar@gmail.com>
2022-10-19 11:58:16 +0530

    final changes

---
+++ Sample-ques-06/app.js
@@ -0,0 +1,6 @@
+ var text = document.querySelector("#demoText")
+ var submitBtn = document.querySelector("#loadBtn")
+
+ submitBtn.addEventListener("click",()=>{
+    text.style.display = "none"
+ })
