6ecd1be8aeae3667d75056729a075dd2c0b1f2da
RuchaRK <ruchakathar@gmail.com>
2022-10-20 08:53:29 +0530

    final changes

---
+++ Border-resizer/app.js
@@ -0,0 +1,17 @@
+var name1 = document.querySelector("#nameInput")
+var firstBtn = document.querySelector("#firstBtn")
+var secondBtn = document.querySelector("#secondBtn")
+var thirdBtn = document.querySelector("#thirdBtn")
+
+firstBtn.addEventListener("click",()=>
+{
+    name1.style.borderWidth = "5px"
+})
+
+secondBtn.addEventListener("click",()=>{
+    name1.style.borderWidth = "10px"
+})
+
+thirdBtn.addEventListener("click",()=>{
+    name1.style.borderWidth = "1px"
+})
